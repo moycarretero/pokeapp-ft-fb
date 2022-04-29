e624e69707350d7799a55daf36e8492fe4bd39d7
AlvaroGutierrez96 <alvaro.gutierrez@bootcamp-upgrade.com>
2022-04-29 17:21:48 +0200

    security

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -7,6 +7,7 @@ use App\Entity\Pokemon;
 use App\Form\PokemonType;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -106,6 +107,7 @@ class PokemonController  extends AbstractController
     }
 
     #[Route("/edit/pokemon/{id}", name: "editPokemon")]
+    #[IsGranted('ROLE_ADMIN')]
     public function editPokemon(Request $request, EntityManagerInterface $doctrine, $id)
     {
 
---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+    
+    #[Route(path: '/insert/user', name: 'insert_user')]
+
+    public function insertUser(EntityManagerInterface $doctrine, UserPasswordHasherInterface $hasher){
+        $user = new User();
+        $user-> setUsername('admin');
+        $user-> setPassword($hasher->hashPassword($user, '1234'));
+        $user->setRoles([
+            'ROLE_ADMIN',
+            'ROLE_USER'
+        ]);
+
+        $doctrine -> persist($user);
+        $doctrine-> flush();
+    }
+}
