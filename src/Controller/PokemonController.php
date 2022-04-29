6b0a8f762da88de752493f26ab8c3e535560ecd2
Moises Carretero <moises.carretero@unidadeditorial.es>
2022-04-29 09:35:08 +0200

    Inserto profiler

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -84,9 +84,23 @@ class PokemonController  extends AbstractController
 
         $doctrine->persist($pokemon1);
         $doctrine->persist($pokemon2);
-        $doctrine->flush(); 
+        $doctrine->flush();
 
-        return new Response("Pokemon insertado correctamente");
+        $pokemon3 = new Pokemon();
+
+        $pokemon3->setName("Caterpie");
+        $pokemon3->setDescription("Cuanto más potente es la energía eléctrica que genera este Pokémon, más suaves y elásticas se vuelven las bolsas de sus mejillas.");
+        $pokemon3->setImage("https://assets.pokemon.com/assets/cms2/img/pokedex/full/010.png");
+        $pokemon3->setCode("010");
+
+        $doctrine->persist($pokemon3);
+
+        $pokemon2->setName("Squirtle");
+
+        $doctrine->flush();
+
+        //return new Response("Pokemon insertado correctamente");
+        return $this->render("base.html.twig");
 
     }
 
